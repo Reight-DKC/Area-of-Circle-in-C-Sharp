58848c46b8e682bda6251956c86ba880cd374809
Reight-DKC <reightultimate@gmail.com>
2022-06-13 11:44:14 -0600

    Area of circle by console

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// Circle Aréa
+Console.WriteLine("pi = 3,14 cm" );
+Console.WriteLine("r = 4 cm");
+
+Console.WriteLine("\nArea = pi * Math.Pow(4, 2)");
+
+Console.WriteLine("\nFour elevated to two is:");
+Console.WriteLine(Math.Pow(4, 2));
+
+Console.WriteLine("\nArea = pi * 16");
+
+Console.WriteLine("\nCircle Area");
+Console.WriteLine(3.14 * 16);
